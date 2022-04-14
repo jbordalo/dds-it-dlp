af0986c2913cff8dc8e045c12021aaee9d40462a
jbordalo <j.bordalo@campus.fct.unl.pt>
2022-04-14 15:20:49 +0100

    Add LedgerRequestType ENUM

---
+++ src/main/java/com/dds/springitdlp/application/bftSmart/LedgerRequestType.java
@@ -0,0 +1,10 @@
+package com.dds.springitdlp.application.bftSmart;
+
+public enum LedgerRequestType {
+    SEND_TRANSACTION,
+    GET_LEDGER,
+    GET_BALANCE,
+    GET_EXTRACT,
+    GET_TOTAL_VALUE,
+    GET_GLOBAL_LEDGER_VALUE
+}
