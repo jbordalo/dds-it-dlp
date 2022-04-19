0633336b7c408c69b98c46c4db8adddd5b3d29fa
Jace <jlo.sousa@campus.fct.unl.pt>
2022-04-19 16:22:06 +0100

    Update run script and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM openjdk:17
 ENV KEYSTORE_PASSWORD=dds-it-dlp
-EXPOSE 11000 11010 11020 11030
 COPY build/libs/*.jar app.jar
 COPY config config
 COPY config/hosts-docker.config config/hosts.config
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 docker network remove dds-network
 docker network create -d bridge dds-network --subnet=172.26.0.0/16
-gnome-terminal -- docker run -e "PORT=8080" -e "REPLICA_ID=0" --network=dds-network -p 8080:8080 -p 11000:11000 --ip=172.26.0.2 -t jc/dds-it-dlp &
-gnome-terminal -- docker run -e "PORT=8081" -e "REPLICA_ID=1" --network=dds-network -p 8081:8081 -p 11010:11010 --ip=172.26.0.3 -t jc/dds-it-dlp &
-gnome-terminal -- docker run -e "PORT=8082" -e "REPLICA_ID=2" --network=dds-network -p 8082:8082 -p 11020:11020 --ip=172.26.0.4 -t jc/dds-it-dlp &
-gnome-terminal -- docker run -e "PORT=8083" -e "REPLICA_ID=3" --network=dds-network -p 8083:8083 -p 11030:11030 --ip=172.26.0.5 -t jc/dds-it-dlp &
+gnome-terminal -- docker run -e "PORT=8080" -e "REPLICA_ID=0" --network=dds-network -p 8080:8080 --ip=172.26.0.2 -t jc/dds-it-dlp &
+gnome-terminal -- docker run -e "PORT=8081" -e "REPLICA_ID=1" --network=dds-network -p 8081:8081 --ip=172.26.0.3 -t jc/dds-it-dlp &
+gnome-terminal -- docker run -e "PORT=8082" -e "REPLICA_ID=2" --network=dds-network -p 8082:8082 --ip=172.26.0.4 -t jc/dds-it-dlp &
+gnome-terminal -- docker run -e "PORT=8083" -e "REPLICA_ID=3" --network=dds-network -p 8083:8083 --ip=172.26.0.5 -t jc/dds-it-dlp &
 
